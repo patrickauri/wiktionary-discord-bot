22f25774c1c5bfac4c9e3da7c374301a5aa8b2e2
Pato <patrickaulie@gmail.com>
2020-11-27 15:01:22 +0100

    Updated LookUp
    LookUp now replies with the body from the first result

--- api.js
+++ api.js
@@ -1,24 +1,39 @@
 const Discord = require('discord.js');
+const request = require('request');
 require('dotenv').config();
 const client = new Discord.Client();
 const commandPrefix = '!';
 const commandName = 'dict';
 
-const LookUp = (lang, query) => {
-    console.log(`Looking up query: ${query} in language: ${lang}`);
+const LookUp = (lang, word, msg) => {
+    const query = `https://en.wiktionary.org/w/api.php?action=query&list=search&srsearch=${word}&format=json`;
+    console.log(`Query: ${query}`);
+    request(query, { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        if (body.query.searchinfo.totalhits > 0) {
+            const result = body.query.search[0].snippet;
+            console.log(result);
+            msg.channel.send(result);
+        }
+        else {
+            console.log('No Results');
+        }
+    });
 }
 
-const parseInput = (t) => {
-    let x = t.split(' ');
-    console.log(x)
-    if (x.length >= 3) {
-        LookUp(x[1], x[2]);
+const parseInput = (msg) => {
+    let x = msg.content.split(' ');
+    const cmd = commandPrefix + commandName;
+    if (x.length >= 3 && x[0] === cmd) {
+        LookUp(x[1], x[2], msg);
     }
 }
 
 client.on('message', (msg) => {
     if (msg.content[0] === commandPrefix) {
-        parseInput(msg.content);
+        parseInput(msg);
     }
 });
 
